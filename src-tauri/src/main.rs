40a1056b410fb0bc83fd8e1e15a3943f4ac92629
Karavang <k4r4v4ng@gmail.com>
2024-01-25 22:37:28 +0100

    release

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,60 +1,86 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use serde::{Deserialize, Serialize};
 use std::fs::{File, OpenOptions};
 use std::io::{Read, Write};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Data {
+    links: Vec<String>,
+}
+
 #[tauri::command]
-fn new_note(link: String) -> Result<bool, std::io::Error> {
-    let file_name = "data.txt";
+fn new_note(link: String) -> Result<bool, tauri::Error> {
+    let file_name = "data.json";
 
     let mut file = OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
-        .append(true)
         .open(file_name)?;
 
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    contents.push_str(&link);
+    let mut data: Data = if contents.is_empty() {
+        Data { links: Vec::new() }
+    } else {
+        serde_json::from_str(&contents)?
+    };
 
-    file.write_all(contents.as_bytes())?;
+    data.links.push(link);
+
+    let serialized_data = serde_json::to_string(&data)?;
+
+    let mut file = File::create(file_name)?;
+    file.write_all(serialized_data.as_bytes())?;
 
     Ok(true)
 }
+
 #[tauri::command]
-fn count_elements() -> Result<usize, std::io::Error> {
-    let file_name = "data.txt";
+fn count_elements() -> Result<usize, tauri::Error> {
+    let file_name = "data.json";
 
     let mut file = File::open(file_name)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    let elements: Vec<&str> = contents.lines().collect();
-    let count = elements.len();
+    let data: Data = if contents.is_empty() {
+        Data { links: Vec::new() }
+    } else {
+        serde_json::from_str(&contents)?
+    };
 
-    println!("Number of elements in the array: {}", count);
+    let count = data.links.len();
 
     Ok(count)
 }
+
 #[tauri::command]
-fn contains_text(search_text: &str) -> Result<bool, std::io::Error> {
-    let file_name = "data.txt";
+fn contains_text(search_text: &str) -> Result<bool, tauri::Error> {
+    let file_name = "data.json";
 
     let mut file = File::open(file_name)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    let elements: Vec<&str> = contents.lines().collect();
-    let contains = elements.contains(&search_text);
+    let data: Data = if contents.is_empty() {
+        Data { links: Vec::new() }
+    } else {
+        serde_json::from_str(&contents)?
+    };
+
+    let contains = data.links.contains(&search_text.to_string());
 
     Ok(contains)
 }
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
-            new_note as fn(String) -> Result<bool, std::io::Error>,
-            count_elements as fn() -> Result<usize, std::io::Error>,
-            contains_text as fn(&str) -> Result<bool, std::io::Error>
+            new_note,
+            count_elements,
+            contains_text
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,65 @@
-import { useEffect, useState } from "react";
-import "./App.css";
 import { invoke } from "@tauri-apps/api";
+import { useState, useEffect } from "react";
+import "./App.css";
+
 function App() {
   const [note, setNote] = useState("");
-  const total = async () => {
-    return await invoke("count_elements");
+  const [totalValue, setTotalValue] = useState(0);
+  const [isContain, setIsContain] = useState(false);
+  const total = async () =>
+    setTotalValue(await invoke("count_elements").catch(console.error));
+
+  const contains_text = async () =>
+    setIsContain(
+      await invoke("contains_text", { searchText: note }).catch(console.error),
+    );
+
+  const newNote = async (e) => {
+    e.preventDefault();
+    await invoke("new_note", { link: note });
+    total();
   };
-  const newNote = async () => {
-    return await invoke("new_note", { link: note });
-  };
+
+  useEffect(() => {
+    const inputVac = document.getElementById("input-vac");
+    const buttonVac = document.getElementById("button-vac");
+    if (isContain) {
+      inputVac.style.borderColor = "green";
+      buttonVac.setAttribute("disabled", "true");
+    } else {
+      inputVac.style.borderColor = "red";
+      buttonVac.removeAttribute("disabled");
+    }
+  }, [isContain]);
   useEffect(() => {
     total();
   }, []);
+
+  useEffect(() => {
+    contains_text(note);
+  }, [note]);
+
   return (
     <>
       <h1>My checked vacances</h1>
       <div className="flexator">
-        <div>
+        <form onSubmit={newNote}>
           <input
+            id="input-vac"
             type="text"
-            onChange={(e) => setNote(e.target.value)}
+            onChange={(e) => {
+              const value = e.target.value;
+              setNote(value);
+            }}
           />
-          <button onClick={newNote}>Save</button>
-        </div>
-        <p>Total: {total}</p>
+          <button
+            type="submit"
+            id="button-vac"
+          >
+            Save
+          </button>
+        </form>
+        <p>Total: {totalValue}</p>
       </div>
     </>
   );
